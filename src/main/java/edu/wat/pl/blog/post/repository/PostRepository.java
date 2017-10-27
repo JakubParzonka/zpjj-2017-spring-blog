340a86f53d927655e3cc0eab71d675e4437871c7
JakubParzonka <jakub.parzonka@outlook.com>
2017-10-27 22:26:41 +0200

    Saving and reading posts and titles db connection

---
+++ src/main/java/edu/wat/pl/blog/post/repository/PostRepository.java
@@ -0,0 +1,12 @@
+package edu.wat.pl.blog.post.repository;
+
+import edu.wat.pl.blog.post.Post;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.List;
+
+public interface PostRepository extends MongoRepository<Post, String>/*, QueryDslPredicateExecutor<Post> */ {
+
+    List<Post> findPostByTitle(String s);
+
+}
